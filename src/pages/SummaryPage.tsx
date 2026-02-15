574c90db592983eef68d11219591227083ced6f9
Asanali Nurmagambet <nurmaganbetasanali7@gmail.com>
2026-02-15 14:33:35 +0500

    style: add highlighted icon before create flashcards button label

--- src/pages/SummaryPage.tsx
+++ src/pages/SummaryPage.tsx
@@ -845,7 +845,7 @@ export function SummaryPage() {
                   variant="outline"
                   onClick={() => navigate(`/flashcards/create/${id}`)}
                 >
-                  <Layers className="h-4 w-4 mr-2" />
+                  <Layers className="h-4 w-4 mr-2 shrink-0 text-amber-500" />
                   Create Flashcards
                 </Button>
               </div>
